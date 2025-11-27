d1549e7e3fca933783e100da77f35fc8ecfd2520
Arthur_Ferreira <arthurcarvalhof@gmail.com>
2025-11-27 19:01:11 -0300

    Primeiro commit

---
+++ screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,30 @@
+package br.com.alura.screenmatch;
+
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.service.ConveterDados;
+import br.com.alura.screenmatch.service.CosumoApi;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+
+
+	}
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        CosumoApi cosumoApi = new CosumoApi();
+        var json = cosumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=41394108");
+        System.out.println(json);
+        ConveterDados conveterDados = new ConveterDados();
+        DadosSerie dadosSerie = conveterDados.obterDadosJson(json, DadosSerie.class);
+        System.out.println(dadosSerie);
+
+    }
+}
